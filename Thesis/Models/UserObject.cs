3c323e8b5f767ab01ff4cbae959176f821776580
NikitaGordienko <nikitagordienk0@mail.ru>
2019-05-31 19:58:57 +0300

    Фикс модели UserObject
    Явно указано свойство Id в классе модели.

--- Thesis/Models/UserObject.cs
+++ Thesis/Models/UserObject.cs
@@ -7,10 +7,13 @@ namespace Thesis.Models
 {
     public class UserObject
     {
+        public string Id { get; set; }
+
         public string UserId { get; set; }
         public User User { get; set; }
 
         public string ObjectId { get; set; }
         public Object Object { get; set; }
+
     }
 }
